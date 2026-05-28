7a6a98f085108ad2d3f24e686d8ebbc8ecfa7602
Gurusb <gurannagouda.biradar@sarla-aviation.com>
2026-05-28 05:58:10 +0000

    checked linux server-vs code

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main() {
     printf("Hello this is modified code GitHub\n");
     printf("this is in dev branch");
     printf("this is in dev branch, from vs code editor on windows");    
+    pritnf("this is in dev branch, from vs code editor on windows, second commit");
     return 0;
 }
